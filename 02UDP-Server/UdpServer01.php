8402cd2f87801b32c661ff10b78336f98d90ecde
Goular <zhaojt_cs@126.com>
2017-09-18 23:05:09 +0800

    提交内容

--- 02UDP-Server/UdpServer01.php
+++ 02UDP-Server/UdpServer01.php
@@ -6,10 +6,11 @@ $serv = new swoole_server("0.0.0.0", 7070, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
  * $data : 数据，接收到的信息
  * $fd : 客户端信息
  */
-$serv->on('packet', function (swoole_server $serv, $data, $fd) {
+$serv->on('packet', function ($serv, $data, $fd) {
     //发送数据到相应的客户端，反馈消息
     //$serv->sendto($fd['address'], $fd['port'], "Server:" + $data);
     var_dump($serv);
     var_dump($fd);
+    var_dump($data);
 });
 $serv->start();

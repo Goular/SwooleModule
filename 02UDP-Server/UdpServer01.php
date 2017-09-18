bdce86f5e2fe58a3bf2643dd73dfa7ee7ff82027
Goular <zhaojt_cs@126.com>
2017-09-18 22:50:59 +0800

    提交内容

---
+++ 02UDP-Server/UdpServer01.php
@@ -0,0 +1,14 @@
+<?php
+$serv = new swoole_server("0.0.0.0", 7070, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+//监听数据接收的事件
+/**
+ * $serv : 服务器信息
+ * $data : 数据，接收到的信息
+ * $fd : 客户端信息
+ */
+$serv->on('packet', function (swoole_server $serv, $data, $fd) {
+    //发送数据到相应的客户端，反馈消息
+    $serv->sendto($fd['address'], $fd['port'], "Server:" + $data);
+    var_dump($fd);
+});
+$serv->start();

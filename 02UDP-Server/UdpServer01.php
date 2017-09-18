112cfd612491ea6a9ce56c74498a0deec63a1858
Goular <zhaojt_cs@126.com>
2017-09-18 23:51:48 +0800

    提交内容

--- 02UDP-Server/UdpServer01.php
+++ 02UDP-Server/UdpServer01.php
@@ -1,6 +1,6 @@
 <?php
 //创建Server对象，监听 127.0.0.1:9502端口，类型为SWOOLE_SOCK_UDP
-$serv = new swoole_server("0.0.0.0", 7070, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
+$serv = new swoole_server("0.0.0.0", 7075, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
 //监听数据接收事件
 $serv->on('Packet', function ($serv, $data, $clientInfo) {

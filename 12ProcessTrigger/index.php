1a0d80bad89f027a6ea4f8f0916cde82e870deb8
Goular <zhaojt_cs@126.com>
2017-09-19 17:43:40 +0800

    提交内容

--- 12ProcessTrigger/index.php
+++ 12ProcessTrigger/index.php
@@ -1,6 +1,17 @@
 <?php
 //触发函数 异步执行 执行到10自动停止
+//swoole_process::signal(SIGALRM, function () {
+//    echo "1\n";
+//});
+//swoole_process::alarm(100 * 1000);
+
+
 swoole_process::signal(SIGALRM, function () {
-    echo "1\n";
+    static $i = 0;
+    echo "$i\n";
+    $i++;
+    if ($i > 10) {
+        swoole_process::alarm(-1);
+    }
 });
 swoole_process::alarm(100 * 1000);

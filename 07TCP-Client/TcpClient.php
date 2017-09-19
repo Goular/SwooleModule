327ab35c6e56630b3d03514136b7ee86796b4af6
Goular <zhaojt_cs@126.com>
2017-09-19 15:21:21 +0800

    提交内容

---
+++ 07TCP-Client/TcpClient.php
@@ -0,0 +1,18 @@
+<?php
+$client = new swoole_client(SWOOLE_SOCK_TCP);
+//连接到服务器
+if (!$client->connect('0.0.0.0', 7070, 1)) {
+    die("Connect failed!");
+}
+//向服务器发送数据
+if ($client->send("Hello Sync Tcp Client!")) {
+    die("Send failed！");
+}
+//从服务器接收数据
+$data = $client->recv();
+if (!$data) {
+    die("Receive failed！");
+}
+echo $data;
+//关闭连接
+$client->close();

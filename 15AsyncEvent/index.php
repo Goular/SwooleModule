d176a6333232a04e1c005475d662710ccd2b4746
Goular <zhaojt_cs@126.com>
2017-09-20 09:52:35 +0800

    提交内容

---
+++ 15AsyncEvent/index.php
@@ -0,0 +1,10 @@
+<?php
+$fp = stream_socket_client("tcp://www.qq.com:80", $errorno, $errstr, 30);
+fwrite($fp, "GET / HTTP/1.1\r\nHost:www.qq.com\r\n\r\n");
+//添加异步事件
+swoole_event_add($fp, function ($fp) {
+    $resp = fread($fp, 8192);
+    var_dump($resp);
+    fclose($fp);
+});
+echo "这个先执行完成";

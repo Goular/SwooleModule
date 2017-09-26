342e305af380ed99184c02d663f1ec4d73f2647a
Goular <zhaojt_cs@126.com>
2017-09-26 10:50:17 +0800

    提交内容

--- swoole-cource/07Protocols&Ports/swoole_stable_header_client.php
+++ swoole-cource/07Protocols&Ports/swoole_stable_header_client.php
@@ -19,6 +19,7 @@ class Client
         }
         $msg_normal = "This is a Msg.";
         $msg_length = pack("N", strlen($msg_normal)) . $msg_normal;
+        echo "\n".$msg_length."\n";
         $i = 0;
         while ($i < 100) {
             $this->client->send($msg_length);

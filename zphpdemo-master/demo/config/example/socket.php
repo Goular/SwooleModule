e048c54bf632a09eb42348fde53d5044242f4849
Goular <zhaojt_cs@126.com>
2017-09-26 23:33:02 +0800

    提交内容

--- zphpdemo-master/demo/config/example/socket.php
+++ zphpdemo-master/demo/config/example/socket.php
@@ -3,11 +3,11 @@
     return array(
         'socket'=>array(
             'host'=>'0.0.0.0',                          //socket 监听ip
-            'port'=>8991,                               //socket 监听端口
+            'port'=>7070,                               //socket 监听端口
             'adapter'=>'React',                          //socket 驱动模块
             'daemonize'=>0,                             //是否开启守护进程
             'times'=>array(),                           //定时服务
-            'client_class'=>'socket\\React',            //socket 回调类
+            'client_class'=>'socket\\Swoole',            //socket 回调类
             'protocol'=>'Json',                         //socket通信数据协议
             'call_mode'=>'ZPHP',                        //业务处理模式
             'work_mode'=>3,                             //工作模式：1：单进程单线程 2：多线程 3： 多进程

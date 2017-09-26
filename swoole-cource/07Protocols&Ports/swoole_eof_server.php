e672c895a9512b949eda86f60784e22b36d3a595
Goular <zhaojt_cs@126.com>
2017-09-26 10:26:47 +0800

    提交内容

--- swoole-cource/07Protocols&Ports/swoole_eof_server.php
+++ swoole-cource/07Protocols&Ports/swoole_eof_server.php
@@ -16,8 +16,8 @@ class server
             'max_request' => 10000,
             'dispatch_mode' => 2,
             'package_max_length' => 8192,
-            'open_eof_check' => true,
-            //'open_eof_split' => true,
+            //'open_eof_check' => true,
+            'open_eof_split' => true,
             'package_eof' => "\r\n"
         ]);
         $this->serv->on("Start", array($this, "onStart"));
@@ -43,12 +43,12 @@ class server
         var_dump($data);
         //这个是配置参数:open_eof_check专用，因为整个数据包是从后往前读，只要遇到分隔符就直接返回，
         //这样前面的分隔符就会被忽略同时全部返回，这样不好，所以我们需要在这里进行分隔，open_eof_split不会出现这个问题
-        $data_list = explode("\r\n", $data);
-        foreach ($data_list as $msg) {
-            if (!empty($msg)) {
-                echo "Get Message From Client {$fd}:{$msg}\n";
-            }
-        }
+//        $data_list = explode("\r\n", $data);
+//        foreach ($data_list as $msg) {
+//            if (!empty($msg)) {
+//                echo "Get Message From Client {$fd}:{$msg}\n";
+//            }
+//        }
     }
 
     public function onClose(swoole_server $serv, $fd, $from_id)

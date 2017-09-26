6ad3893d06ae0c2749f545da0f462d856194976f
Goular <zhaojt_cs@126.com>
2017-09-26 16:40:37 +0800

    提交内容

---
+++ swoole-cource/09hprose/webroot/main.php
@@ -0,0 +1,5 @@
+<?php
+use ZPHP\ZPHP;
+$rootPath = dirname(__DIR__);
+require 'F:\GitHub\SwooleModule\swoole-cource\09hprose\vendor\zphp\zphp'. DIRECTORY_SEPARATOR . 'ZPHP' . DIRECTORY_SEPARATOR . 'ZPHP.php';
+ZPHP::run($rootPath);
